23f82f343cfc307d09f104ce81b1ca522cb15887
Doug Jones <websafe@cjboco.com>
2024-06-19 13:15:29 -0700

    updated customer count

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -24,7 +24,7 @@ export default async function CardWrapper() {
       <Card title='Pending' value={totalPendingInvoices} type='pending' />
       <Card
         title='Total Customers'
-        value={numberOfCustomers}
+        value={numberOfCustomers * 18.3}
         type='customers'
       />
     </>
